62d4ad14316129e9f0a49dc1d8dcc11401653fee
sahokawamura <saho_kawamura@fitnyc.edu>
2020-10-27 22:55:46 +0900

    modified index page

--- server/resources/views/articles/index.blade.php
+++ server/resources/views/articles/index.blade.php
@@ -1,4 +1,4 @@
-<h1>論文一覧</h1>
+{{-- <h1>論文一覧</h1>
 
 <ul style="list-style-type:none; padding-left:0">
     @foreach ($articles as $article)
@@ -6,4 +6,14 @@
     @endforeach
 </ul>
 
+<a href="/articles/create"><button>新規論文投稿</button></a> --}}
+
+<h1>論文一覧</h1>
+
+@foreach ($articles as $article)
+    <p><a href="/articles/{{ $article->id }}">{{ $article->title }}</a></p>
+@endforeach
+
+
 <a href="/articles/create"><button>新規論文投稿</button></a>
+
